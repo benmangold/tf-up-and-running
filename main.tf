432e0ee3da07d3186924d85218e41dfe0052cd19
benmangold <benmangold@gmail.com>
2020-08-23 20:32:59 -0400

    variable and output p60

--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@ provider "aws" {
     region = "us-east-2"
 }
 
+variable "server_port" {
+    description = "The port used by the server for HTTP requests"
+    type = number
+    default = 8080
+}
+
 resource "aws_instance" "example" {
     ami = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
@@ -21,10 +27,15 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
     ingress {
-        from_port = 8080
-        to_port = 8080
+        from_port = var.server_port
+        to_port = var.server_port
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+}
+
+output "public_ip" {
+    value = aws_instance.example.public_ip
+    description = "The public ip address of the web server"
 }
 
